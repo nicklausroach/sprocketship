ae2da0d545f33531ab269876f4db8bc40a4f3848
Nick Roach <nickroach@dagsterlabs.com>
2024-04-06 16:00:21 -0400

    more updates to example procs

--- sprocketship/example/procedures/development/create_temp_database.js
+++ sprocketship/example/procedures/development/create_temp_database.js
@@ -3,16 +3,35 @@ var userResult = snowflake.execute({sqlText: `SELECT CURRENT_USER()`});
 userResult.next();
 
 var currentUser = userResult.getColumnValue(`CURRENT_USER()`);
-var databaseName = DATABASE_NAME;
+var databaseName = DATABASE_NAME.toUpperCase();
+var ttl = parseInt(TTL);
+
+const scheduledDropTimestamp = new Date();
+scheduledDropTimestamp.setDate(scheduledDropTimestamp.getDate() + ttl);
 
 var sqlCommands = [
     `CREATE DATABASE ${databaseName}`,
     `CALL SANDBOX.NICKROACH.CREATE_DATABASE_WRITER('${databaseName}');`,
     `CALL SANDBOX.NICKROACH.CREATE_DATABASE_READER('${databaseName}');`,
     `GRANT ROLE ${databaseName}_READER, ${databaseName}_WRITER TO USER ${currentUser};`,
-    `GRANT ROLE ${databaseName}_READER, ${databaseName}_WRITER TO ROLE ACCOUNTADMIN;`
+    `GRANT ROLE ${databaseName}_READER, ${databaseName}_WRITER TO ROLE ACCOUNTADMIN;`,
+    `INSERT INTO UTIL_DB.PROC_TABLES.TEMP_DATABASES (
+        DATABASE_NAME,
+        CREATED_BY,
+        CREATED_AT,
+        TTL,
+        SCHEDULED_DROP_TIMESTAMP
+    ) VALUES
+        (
+            '${databaseName}',
+            '${currentUser}',
+            '${new Date().toISOString()}',
+            ${ttl},
+            '${scheduledDropTimestamp.toISOString()}'
+        )
+    `
 ]
 
 var resultSet = sqlCommands.map(command => snowflake.execute({sqlText: command}));
 
-return`Database ${databaseName.toUpperCase()} created successfully.`;
+return`Database ${databaseName} created successfully.`;
